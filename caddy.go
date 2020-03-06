ac6cbe925e9d3d3cb3d157ba9f0a21f0df399a60
jmb12686 <jmb186@gmail.com>
2020-03-06 13:50:50 -0500

    Add more plugins to caddy

--- caddy.go
+++ caddy.go
@@ -7,9 +7,14 @@ import (
 	// _ "import/path/here"
 	// _ "github.com/tarent/loginsrv/tree/master/caddy"
 	_ "github.com/BTBurke/caddy-jwt"
+	_ "github.com/abiosoft/caddy-git"
 	_ "github.com/caddyserver/dnsproviders/cloudflare"
 	_ "github.com/captncraig/caddy-realip"
+	_ "github.com/captncraig/cors"
 	_ "github.com/echocat/caddy-filter"
+	_ "github.com/epicagency/caddy-expires"
+	_ "github.com/nicolasazrak/caddy-cache"
+	_ "github.com/pyed/ipfilter"
 	_ "github.com/tarent/loginsrv/caddy"
 )
 
